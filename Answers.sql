8a5f33954046759313f93a0c2b4275993d0979ff
ian ndiga <ianndiga0@gmail.com>
2025-10-17 17:51:43 +0300

    add

--- Answers.sql
+++ Answers.sql
@@ -89,3 +89,36 @@ GRANT INSERT ON salesDB.* TO 'bob'@'localhost';
 
 -- Question 4
 ALTER USER 'bob'@'localhost' IDENTIFIED BY 'P$55!23';
+
+
+-- week 6
+-- Question 1
+SELECT 
+    e.firstName, 
+    e.lastName, 
+    e.email, 
+    e.officeCode
+FROM employees AS e
+INNER JOIN offices AS o
+ON e.officeCode = o.officeCode;
+
+-- Question 2
+SELECT 
+    p.productName, 
+    p.productVendor, 
+    p.productLine
+FROM products AS p
+LEFT JOIN productlines AS pl
+ON p.productLine = pl.productLine;
+
+-- Question 3
+SELECT 
+    o.orderDate, 
+    o.shippedDate, 
+    o.status, 
+    o.customerNumber
+FROM customers AS c
+RIGHT JOIN orders AS o
+ON c.customerNumber = o.customerNumber
+LIMIT 10;
+
