fa05110046e98582846e218a71fda696f54a0816
ian ndiga <ianndiga0@gmail.com>
2025-10-17 17:21:56 +0300

    add

--- Answers.sql
+++ Answers.sql
@@ -53,3 +53,26 @@ VALUES
 UPDATE student
 SET age = 20
 WHERE id = 2;
+
+-- Week 4
+-- Question 1
+SELECT paymentDate, SUM(amount) AS totalAmount
+FROM payments
+GROUP BY paymentDate
+ORDER BY paymentDate DESC
+LIMIT 5;
+
+-- Question 2
+SELECT customerName, country, AVG(creditLimit) AS averageCreditLimit
+FROM customers
+GROUP BY customerName, country;
+
+-- Question 3
+SELECT productCode, quantityOrdered, (quantityOrdered * priceEach) AS totalPrice
+FROM orderdetails
+GROUP BY productCode, quantityOrdered, priceEach;
+
+-- Question 4
+SELECT checkNumber, MAX(amount) AS highestAmount
+FROM payments
+GROUP BY checkNumber;
