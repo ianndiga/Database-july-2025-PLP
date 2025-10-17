0f118986afc9187dee8bebbdcf6c2accfec1f5d5
ian ndiga <ianndiga0@gmail.com>
2025-10-17 17:41:45 +0300

    add

--- Answers.sql
+++ Answers.sql
@@ -76,3 +76,16 @@ GROUP BY productCode, quantityOrdered, priceEach;
 SELECT checkNumber, MAX(amount) AS highestAmount
 FROM payments
 GROUP BY checkNumber;
+
+-- Week 5
+-- Question 1
+DROP INDEX IdxPhone ON customers;
+
+-- Question 2
+CREATE USER 'bob'@'localhost' IDENTIFIED BY 'S$cu3r3!';
+
+-- Question 3
+GRANT INSERT ON salesDB.* TO 'bob'@'localhost';
+
+-- Question 4
+ALTER USER 'bob'@'localhost' IDENTIFIED BY 'P$55!23';
